3e56539c47992d4698cdcf7d45a39211a60f5fd5
Vitaly Cherkov <vv-ch@bk.ru>
2017-11-04 19:32:11 +0300

    Пагинация и по тегам и по постам. Нужно допилить для категорий

--- post/views.py
+++ post/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.core.paginator import Paginator
 from .models import Post
 from comment.models import Comment
 from comment.views import CommentsList
@@ -18,7 +18,8 @@ class DetailPost(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(DetailPost, self).get_context_data()
-        context[CommentsList.context_object_name] = Comment.user_comments.get_queryset(self.kwargs['pk'])
+        context[CommentsList.context_object_name] = \
+            Comment.user_comments.get_queryset(self.kwargs['pk'])
         return context
 
 
@@ -26,4 +27,19 @@ class ListPost(ListView):
     model = Post
     template_name = 'post/list.html'
     context_object_name = 'posts_list'
+    paginate_by = 10
 
+    def set_paginator_options(context):
+        first_index = max(context['page_obj'].number - 3, 1)
+        last_index = min(first_index + 6, context['paginator'].num_pages)
+        first_index = max(1, last_index - 6)
+
+        return {
+            'first': first_index,
+            'last': last_index
+        }
+
+    def get_context_data(self, **kwargs):
+        context = super(ListPost, self).get_context_data(**kwargs)
+        context['pag_options'] = ListPost.set_paginator_options(context)
+        return context
--- tag/views.py
+++ tag/views.py
@@ -1,23 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import SingleObjectMixin
 from django.views.generic.detail import DetailView
 from .models import Tag
 from post.models import Post
+from post.views import ListPost
+
 
 def OK(request, **kwargs):
     return render(request, 'OK_user')
 
-
+"""
 class PostsByTag(DetailView):
     model = Tag
     context_object_name = 'tag'
     template_name = 'tag/posts-by-tag.html'
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super(PostsByTag, self).get_context_data()
         context['posts_list'] = Post.posts.get_queryset_by_tag(self.kwargs['pk'])
         return context
+"""
+
+
+class PostsByTag(SingleObjectMixin, ListView):
+    model = Tag
+    template_name = "tag/posts-by-tag.html"
+    paginate_by = 10
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=Tag.objects.all())
+        return super(PostsByTag, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(PostsByTag, self).get_context_data(**kwargs)
+        context['pag_options'] = ListPost.set_paginator_options(context)
+        context['tag_label'] = self.object.label
+        return context
+
+    def get_queryset(self):
+        return Post.posts.get_queryset_by_tag(self.object.pk)
+
 
 
 class PopularTags(ListView):
