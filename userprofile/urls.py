d90086b6d3582b51c09cb3aa5b26f850ec8cab3b
Vitaly Cherkov <vv-ch@bk.ru>
2018-01-04 12:43:35 +0300

    Переделаны LoginView и LogoutView на дефолтные

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls import url
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 from . import views
 
 app_name = 'userprofile'
 urlpatterns = [
     url(r'^(?P<pk>\d+)/', views.OK, name='userprofile-page'),
-    url(r'^login/$', views.LoginView.as_view(), name='login'),
-    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+    url(r'^login/$',
+        LoginView.as_view(
+            redirect_authenticated_user=True,
+            template_name='userprofile/login.html'),
+        name='login'
+        ),
+    url(r'^logout/$', LogoutView.as_view(), name='logout')
 ]
