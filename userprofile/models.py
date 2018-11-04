7b4a44c7b140983acd1ea05485d9ea1008dec4c5
VitalyCherkov <vv-ch@bk.ru>
2018-11-04 15:45:37 +0300

    add avatar field to the model

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,12 +1,25 @@
 from django.shortcuts import reverse, get_object_or_404
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return 'uploads/%s/%s/%s' % (filename[:1], filename[2:3], filename)
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile')
     rating = models.IntegerField(default=0)
     about = models.TextField(blank=True)
+
+    avatar = models.ImageField(upload_to=get_file_path, blank=True, null=True)
+
+    def get_avatar_url(self):
+        try:
+            return self.avatar.url
+        except Exception:
+            return 'static/img/user_bg.jpg'
 
     def get_absolute_url(self):
         return reverse('userprofile:userpage', kwargs={'pk': self.pk})
